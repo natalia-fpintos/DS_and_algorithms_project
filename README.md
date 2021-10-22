29a54430643436499ca06f352911efb64fef6cb0
Natalia Fernández <natalia.fpintos@gmail.com>
2021-10-22 18:39:39 +0100

    Remove unused Flask and start Reade

---
+++ README.md
@@ -0,0 +1,7 @@
+# Project for Data Structures and Algorithms
+
+## Set up
+
+1. Clone the project 
+1. Ensure you are at the `DS_and_algorithms_project` directory in your shell
+1. Run `pip install -r requirements.txt`
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import logging
 import sys
-from flask import Flask
 
 from app.client import Client
 from app.store import Store
@@ -8,14 +7,10 @@ from app.store import Store
 LOG = logging.getLogger(__name__)
 logging.basicConfig(stream=sys.stdout, format='[%(levelname)s] %(message)s', level=logging.INFO)
 
-app = Flask(__name__)
-
-LOG.info("Starting Store application...")
-store = Store('./files/store.txt', 47)
-
-
 if __name__ == '__main__':
+    LOG.info("Starting Store application...")
     print("\n~~~ Welcome to the online store ~~~")
+    store = Store('./files/store.txt', 47)
 
     client = Client(store)
     client.select_options()
